36df4c4ab1510a29575e857c26ffa3bb33f69116
Ed Dye <ed-dye@tampabay.rr.com>
2020-05-19 13:42:48 -0400

    modified code

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-// Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.onload = () => {
+   let launchForm = document.querySelector("form");
+
+   launchForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotInput = document.querySelector("input[name=pilotName]").value;
+      let copilotInput = document.querySelector("input[name=copilotName]").value;
+      let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+      let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuel = document.getElementById("fuelStatus");
+      let cargo = document.getElementById("cargoStatus");
+
+      let validInput = document.querySelectorAll(".textField");
+      let missingFields = [];
+      for (let i=0; i < validInput.length; i++) {
+         if (validInput[i].value === "") {
+            console.log(222);
+            missingFields.push(validInput[i].name);
+         } else if (["pilotName", "copilotName"].includes(event.target[i].name)){
+            if (!isNaN(Number(validInput[i].value))) {
+               missingFields.push(`${validInput[i].name} is a number`);
+            } 
+         } else if (["fuelLevel", "cargoMass"].includes(event.target[i].name)){
+            if (isNaN(validInput[i].value)) {
+               missingFields.push(`${validInput[i].name} is a string`);
+            }
+         }
+      }
+      
+      if (missingFields.length > 0) {
+         alert(`All fields are required!\nMissing fields: ${JSON.stringify(missingFields)}`);
+         event.preventDefault();
+         missingFields = [];
+      } else {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         pilotStatus.innerHTML = `Pilot ${pilotInput} is ready for launch`;
+         copilotStatus.innerHTML = `Co-pilot ${copilotInput} is ready for launch`;
+         if (fuelLevel < 10000) {
+            fuel.innerHTML = "Fuel level too low for launch";
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            launchStatus.style.color = "red";
+         }
+         if (cargoLevel > 10000) {
+            cargo.innerHTML = "There is too much mass for launch";
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            launchStatus.style.color = "red";
+         }       
+      
+      
+      }
+   });
+}
