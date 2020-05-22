1eaedcfe830bf1ec6516ccaea4014851477bef53
Ed Dye <ed-dye@tampabay.rr.com>
2020-05-22 17:06:29 -0400

    Code complted

--- script.js
+++ script.js
@@ -13,17 +13,11 @@ window.onload = () => {
       let copilotStatus = document.getElementById("copilotStatus");
       let fuel = document.getElementById("fuelStatus");
       let cargo = document.getElementById("cargoStatus");
-      class textField {
-         constructor(pilotName, copilotName, fuelLevel, cargoMass) {
-            this.pilotName = pilotName;
-            this.copilotName = copilotName;
-            this.fuelLevel = fuelLevel;
-            this.cargoMass = cargoMass;
-         }
-      }
 
       let validInput = document.querySelectorAll(".textField");
       let missingFields = [];
+
+
       for (let i=0; i < validInput.length; i++) {
          if (validInput[i].value === "") {
             missingFields.push(validInput[i].name);
@@ -34,10 +28,12 @@ window.onload = () => {
          } else if (["fuelLevel", "cargoMass"].includes(event.target[i].name)){
             if (isNaN(validInput[i].value)) {
                missingFields.push(`${validInput[i].name} is a string`);
+            } else {
+
             }
          }
       }
-      
+      let ready = false;
       if (missingFields.length > 0) {
          alert(`All fields are required!\nMissing fields: ${JSON.stringify(missingFields)}`);
          missingFields = [];
@@ -49,14 +45,44 @@ window.onload = () => {
             fuel.innerHTML = "Fuel level too low for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "red";
+         } else {
+            fuel.innerHTML = "Fuel level high enough for launch";
+            launchStatus.innerHTML = "Shuttle Ready for Launch";
+            launchStatus.style.color = "green";
+            ready = true;
          }
          if (cargoLevel > 10000) {
             cargo.innerHTML = "There is too much mass for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "red";
-         }       
+         } else {
+            cargo.innerHTML = "Cargo mass low enough for launch";
+            launchStatus.innerHTML = "Shuttle Ready for Launch";
+            launchStatus.style.color = "green";
+            ready = true;
+         }    
+           
+      }
       
-      
+      let json = [];
+      index = 2; // % json.length;
+      if (ready === true) {
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+            response.json().then(function(json) {
+               const missionTarget = document.getElementById("missionTarget");
+               missionTarget.innerHTML = `
+                  <h2>Mission Destination</h2>
+                  <ol>
+                     <li>Name: ${json[index].name}</li>
+                     <li>Diameter: ${json[index].diameter}</li>
+                     <li>Star: ${json[index].star}</li>
+                     <li>Distance from Earth: ${json[index].distance}</li>
+                     <li>Number of Moons: ${json[index].moons}</li>
+                  </ol>
+                  <img src="${json[index].image}">
+               `;
+            });
+         });
       }
    });
 }
